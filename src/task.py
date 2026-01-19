58c37c566d773e4201ef4a6d768081555837a7f8
Sergei Pavlov <pavlov.aero@gmail.com>
2026-01-19 11:05:55 +0300

    HW_14.2 after tutor check

--- src/task.py
+++ src/task.py
@@ -92,7 +92,6 @@ if __name__ == "__main__":
 
     print(category1.name == "Смартфоны")
     print(category1.description)
-    print(len(category1.products))
     print(category1.category_count)
     print(category1.product_count)
 
@@ -103,7 +102,6 @@ if __name__ == "__main__":
 
     print(category2.name)
     print(category2.description)
-    print(len(category2.products))
     print(category2.products)
 
     print(Category.category_count)
--- tests/test_task.py
+++ tests/test_task.py
@@ -1,11 +1,11 @@
-def test_task_init(product_1):
+def test_task_product_init(product_1):
     assert product_1.name == "Samsung Galaxy S23 Ultra"
     assert product_1.description == "256GB, Серый цвет, 200MP камера"
     assert product_1.price == 180000.0
     assert product_1.quantity == 5
 
 
-def test_task_init(category_1):
+def test_task_category_init(category_1):
     assert category_1.name == "Смартфоны"
     assert category_1.description =="Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
     assert len(category_1.products) == 2
